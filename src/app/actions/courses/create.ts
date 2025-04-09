a2e8fc2a9f1f60497c26240829eede5a17d4c32f
Nico <72673830+NicolasHrt@users.noreply.github.com>
2025-04-09 18:40:34 +0200

    fix: deploy

--- src/app/actions/courses/create.ts
+++ src/app/actions/courses/create.ts
@@ -2,7 +2,7 @@
 
 import { revalidatePath } from "next/cache";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/utils/authOptions";
 import { prisma } from "@/lib/prisma";
 import { ActionState } from "../types";
 
--- src/app/actions/courses/get.ts
+++ src/app/actions/courses/get.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/utils/authOptions";
 import { prisma } from "@/lib/prisma";
 import { ActionState } from "../types";
 
